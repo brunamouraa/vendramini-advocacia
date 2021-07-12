d1e49aae15e17ba7f8fc89c6b775735070083443
brunamouraa <80169032+brunamouraa@users.noreply.github.com>
2021-07-12 16:20:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// ABRE FECHA MENU QUANDO CLICAR NO ICON
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
+}
+
+// FECHAR MENU QUANDO CLICAR NO LINK
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show')
+  })
+}
